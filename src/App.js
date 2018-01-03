7f2ce2734b5a372970cd3c8c47b0921cb3ab8c07
Luke Gaunt <lagaunt120@yahoo.com>
2018-01-03 20:52:29 +0000

    second commit - images added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import questionMark from './images/questionMark.png';
+import entertainment from './images/entertainment.png';
+import general_knowledge from './images/general_knowledge.png';
+import geography from './images/geography.png';
+import history from './images/history.png';
+import science_and_nature from './images/science_and_nature.png';
+import sport from './images/sport.png';
 import './App.css';
 
+const image_paths = [
+  questionMark,
+  general_knowledge,
+  geography,
+  history,
+  science_and_nature,
+  sport
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={image_paths[0]} />
+        <img src={image_paths[1]} />
+        <img src={image_paths[2]} />
+        <img src={image_paths[3]} />
+        <img src={image_paths[4]} />
+        <img src={image_paths[5]} />
+        <img src={image_paths[6]} />
       </div>
     );
   }
