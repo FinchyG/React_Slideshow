f60c09f529414bc27a3ff49953baa78359ac40a3
Luke Gaunt <lagaunt120@yahoo.com>
2018-01-06 15:10:38 +0000

    Working slideshow

---
+++ src/Slideshow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Slideshow extends React.Component {
+    render() {
+      const src = this.props.src;
+      return(
+        <div>
+          <h1>Quiz Crazy</h1>
+          <img src={src} />
+        </div>
+      );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Slideshow} from './Slideshow';
+import questionMark from './images/questionMark.png';
+import entertainment from './images/entertainment.png';
+import general_knowledge from './images/general_knowledge.png';
+import geography from './images/geography.png';
+import history from './images/history.png';
+import science_and_nature from './images/science_and_nature.png';
+import sport from './images/sport.png';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const image_paths = [
+    questionMark,
+    general_knowledge,
+    geography,
+    history,
+    science_and_nature,
+    sport
+  ]
+
+  class SlideshowContainer extends React.Component {
+    
+    constructor(props) {
+      super(props);
+
+      this.state = {currentImage: 0};
+
+      this.interval = null;
+
+      this.nextImage = this.nextImage.bind(this);
+    }
+
+    nextImage() {
+        let current = this.state.currentImage;
+        let next    = ++current % image_paths.length;
+        this.setState({currentImage: next});
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.nextImage, 1000);
+    }
+
+    componentWillMount() {
+        clearInterval(this.interval);
+    }
+    
+    render() {
+        const src= image_paths[this.state.currentImage];
+        return <Slideshow src={src}/>;
+    }
+  }
+
+  ReactDOM.render(<SlideshowContainer />,
+                 document.getElementById('root'));
