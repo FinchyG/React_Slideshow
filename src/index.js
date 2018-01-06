60a83442bac858ce525ce527dc9568d55eba3de5
Luke Gaunt <lagaunt120@yahoo.com>
2018-01-06 15:20:34 +0000

    split image_paths from index file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Slideshow} from './Slideshow';
-import questionMark from './images/questionMark.png';
-import entertainment from './images/entertainment.png';
-import general_knowledge from './images/general_knowledge.png';
-import geography from './images/geography.png';
-import history from './images/history.png';
-import science_and_nature from './images/science_and_nature.png';
-import sport from './images/sport.png';
-
-const image_paths = [
-    questionMark,
-    general_knowledge,
-    geography,
-    history,
-    science_and_nature,
-    sport
-  ]
+import {Image_paths} from './Image_paths';
 
   class SlideshowContainer extends React.Component {
     
@@ -32,7 +17,7 @@ const image_paths = [
 
     nextImage() {
         let current = this.state.currentImage;
-        let next    = ++current % image_paths.length;
+        let next    = ++current % Image_paths.length;
         this.setState({currentImage: next});
     }
 
@@ -45,7 +30,7 @@ const image_paths = [
     }
     
     render() {
-        const src= image_paths[this.state.currentImage];
+        const src= Image_paths[this.state.currentImage];
         return <Slideshow src={src}/>;
     }
   }
